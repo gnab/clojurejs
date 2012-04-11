afd856a84317006f2d7f2fe65d7d4191774d6d81
Ole Petter Bang <olepbang@gmail.com>
2012-04-11 17:59:11 +0200

    Rename parser to reader.

--- test/integration_test.js
+++ test/integration_test.js
@@ -1,9 +1,9 @@
-var parser = require('../src/parser')
+var reader = require('../src/reader')
   , evaluator = require('../src/evaluator')
   ;
 
 describe('Clojure JS', function () {
   it('should perform simple arithmetic operation', function () {
-    evaluator.evaluate(parser.parse('(+ 1 1)')).should.equal(2);
+    evaluator.evaluate(reader.parse('(+ 1 1)')).should.equal(2);
   });
 });
---
+++ test/reader_test.js
@@ -0,0 +1,33 @@
+var reader = require('../src/reader.js')
+  ;
+
+describe('Reader', function () {
+  it('should parse simple expression', function () {
+    reader.parse('(+ 1 1)').should.eql([['+', '1', '1']]);
+  });
+
+  it('should parse nested expression', function () {
+    reader.parse('(+ 1 (* 2 3))').should.eql([['+', '1', ['*', '2', '3']]]);
+  });
+
+  it('should parse function call', function () {
+    reader.parse('(func 1 2)').should.eql([['func', '1', '2']]);
+  });
+
+  it('should ignore spaces', function () {
+    reader.parse('(func   1   2)').should.eql([['func', '1', '2']]);
+  });
+
+  it('should parse strings', function () {
+    reader.parse('(concat "abc" "def")').should.eql([['concat', 'abc', 'def']]);
+  });
+
+  it('should include spaces in strings', function () {
+    reader.parse('(concat "abc def")').should.eql([['concat', 'abc def']]);
+  });
+
+  it('should ignore special characters in strings', function () {
+    reader.parse('(concat "(" ")" "\\"" "\'" \'\\\'\' \'"\')')
+      .should.eql([['concat', '(', ')', '\\"', '\'', '\\\'', '"']]);
+  });
+});
