60d9b196f824a57fdaba7bb357e2e0ab8bc39341
Ole Petter Bang <olepbang@gmail.com>
2012-04-14 21:55:51 +0200

    Refactor token helpers.

--- test/tokens.js
+++ test/tokens.js
@@ -1,11 +1,18 @@
 module.exports = {
-  'n': function (value) { return { kind: 'number', value: value }; }
-, 's': function (value) { return { kind: 'string', value: value }; }
-, 'i': function (value) { return { kind: 'identifier', value: value }; }
-, 'c': function (value) {
-    return { kind: 'call', value: Array.prototype.slice.apply(arguments) };
-  }
-, 'v': function (value) {
-    return { kind: 'vector', value: Array.prototype.slice.apply(arguments) };
-  }
+  'n': token('number')
+, 's': token('string')
+, 'i': token('identifier')
+, 'c': token('call', true)
+, 'v': token('vector', true)
 };
+
+function token (kind, passOnArgs) {
+  return function () {
+    var args = Array.prototype.slice.call(arguments);
+
+    return {
+      kind: kind
+    , value: passOnArgs ? args : args[0]
+    };
+  };
+}
