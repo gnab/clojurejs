c6b5403d00e1108396c34d9b239b07998254608d
Ole Petter Bang <olepbang@gmail.com>
2012-04-29 22:52:13 +0200

    Change demo page into task page supported by Ace editor.

---
+++ tasks.js
@@ -0,0 +1,10 @@
+var tasks = {
+  'FizzBuzz-kata': 
+    '(defn fizzbuzz [n])\n\n' +
+    '(assert (= 1 (fizzbuzz 1)))\n' +
+    '(assert (= "Fizz" (fizzbuzz 3)))\n' +
+    '(assert (= "Buzz" (fizzbuzz 5)))\n' +
+    '(assert (= "Fizz" (fizzbuzz 9)))\n' +
+    '(assert (= "Buzz" (fizzbuzz 10)))\n' +
+    '(assert (= "FizzBuzz" (fizzbuzz 15)))'
+};
