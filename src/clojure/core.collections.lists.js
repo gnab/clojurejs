32ea09a99835ae624c0157bb6dc1a4d88e18d25b
Stig Henriksen <mail@stighenriksen.com>
2012-05-02 23:13:01 +0200

    No need for the .macro=true hacks in collections.lists anymore since quoted lists now aren't evaluated.

--- src/clojure/core.collections.lists.js
+++ src/clojure/core.collections.lists.js
@@ -4,12 +4,11 @@ exports.first = function (list) {
   return evaluator.evaluate([list.value[0]]);
 };
 
-exports.first.macro = true;
-
 exports.second = function (list) {
   return evaluator.evaluate([list.value[1]]);
 };
 
-exports.second.macro = true;
+
+
 
 
