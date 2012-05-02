c857bff37995ff59f2f1826816783a170154d167
Stig Henriksen <mail@stighenriksen.com>
2012-05-02 19:04:51 +0200

    Add arithmetic tasks.

--- tasks.js
+++ tasks.js
@@ -1,11 +1,20 @@
 var tasks = [
   {
     name: 
+      'Simple arithmetic'
+  , description:
+    'Fix the assert by replacing __ with the correct operator.'
+  , code: 
+      '(assert (= 2 (__ 1 1)))\n\n' +
+      '(println "You made it!")'
+  },
+  {
+    name: 
       '1. List operations'
   , description:
-    'Return the first element in the list. Do this by replacing __ with the necessary function call.'
+    'Return the first element in the list.'
   , code: 
-      '(assert (= 5 (__ \'(1 2 3))\n\n' +
+      '(assert (= 5 (__ \'(1 2 3)))\n\n' +
       '(println "You made it!")'
   },
   {
