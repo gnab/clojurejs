0e218e37a7ee0874921d42d2e7d2984775254e09
Stig Henriksen <mail@stighenriksen.com>
2012-05-02 23:21:43 +0200

    Trenger ikke aa haandtere eksplisitt stringify-kall siden dette naa gjores via Token-objektet sin toString-override fra clojurejs.

--- editor.js
+++ editor.js
@@ -20,7 +20,7 @@ function run () {
     result = clojurejs.run(editor.getSession().getValue());
 
     if (result !== undefined) {
-      console.log(result.stringify !== undefined ? result.stringify() : result);
+      console.log(result);
     }
   }
   catch (e) {
