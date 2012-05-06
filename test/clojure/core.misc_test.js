e81b5c79d9559b154bc3a19eccf76aa4102dcb30
Stig Henriksen <mail@stighenriksen.com>
2012-05-06 16:30:28 +0200

    Rename test.

--- test/clojure/core.misc_test.js
+++ test/clojure/core.misc_test.js
@@ -19,7 +19,7 @@ describe('Compare', function () {
   it('= should handle more than two arguments', function () {
     clojure.run('(= 1 2 1 1').should.eql(literal(false));
   });
-  it('= should handle vectors and lists', function () {
+  it('= should equality between vectors and lists', function () {
     clojure.run('(= [1 2 3] \'(1 2 3)').should.eql(literal(true));
     clojure.run('(= [1 2 4] \'(1 2 3)').should.eql(literal(false));
     clojure.run('(= [1 2 3 4] \'(1 2 3)').should.eql(literal(false));
