06cf9eba78bcf60dcd51dcafb087878fc4a34709
Stig Henriksen <mail@stighenriksen.com>
2012-05-06 15:59:09 +0200

    Fix indentation.

--- test/clojure/core.misc_test.js
+++ test/clojure/core.misc_test.js
@@ -4,23 +4,23 @@ var clojure = require('../../src/clojure')
   ;
 
 describe('Compare', function () {
-      it('= should return true if primitives are equal', function () {
-        clojure.run('(= 1 1)').should.eql(literal(true));
-      });
-      it('= should return false if primitives are not equal', function () {
-        clojure.run('(= 1 2)').should.eql(literal(false));
-      });
-      it('= should return true if quoted lists are equal', function () {
-        clojure.run('(= \'(1 2) \'(1 2)').should.eql(literal(true));
-      });
-      it('= should return false if quoted lists are not equal', function () {
-        clojure.run('(= \'(1 2) \'(3 4)').should.eql(literal(false));
-      });
-      it('= should handle more than two arguments', function () {
-        clojure.run('(= 1 2 1 1').should.eql(literal(false));
-      });
-        it('= should handle vectors and lists', function () {
-        clojure.run('(= [1 2 3] \'(1 2 3)').should.eql(literal(true));
-          clojure.run('(= [1 2 4] \'(1 2 3)').should.eql(literal(false));
-      });
+  it('= should return true if primitives are equal', function () {
+    clojure.run('(= 1 1)').should.eql(literal(true));
+  });
+  it('= should return false if primitives are not equal', function () {
+    clojure.run('(= 1 2)').should.eql(literal(false));
+  });
+  it('= should return true if quoted lists are equal', function () {
+    clojure.run('(= \'(1 2) \'(1 2)').should.eql(literal(true));
+  });
+  it('= should return false if quoted lists are not equal', function () {
+    clojure.run('(= \'(1 2) \'(3 4)').should.eql(literal(false));
+  });
+  it('= should handle more than two arguments', function () {
+    clojure.run('(= 1 2 1 1').should.eql(literal(false));
+  });
+  it('= should handle vectors and lists', function () {
+    clojure.run('(= [1 2 3] \'(1 2 3)').should.eql(literal(true));
+    clojure.run('(= [1 2 4] \'(1 2 3)').should.eql(literal(false));
+  });
 });
