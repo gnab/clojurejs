12e9b576e65bc00007c429fda00af1c2e1b491c9
Bobby Towers <porkostomus@mail.com>
2023-07-14 14:26:53 -0700

    just reader

--- src/clojure.js
+++ src/clojure.js
@@ -1,14 +1,6 @@
 import {read} from './reader.js';
-import {evaluate} from './evaluator.js';
 
 export function run (str) {
   var data = read(str);
   return data
-  //return evaluator.evaluate(data);
-}
-
-if (typeof window !== 'undefined') {
-  window.clojurejs = {
-    run: run
-  };
 }
