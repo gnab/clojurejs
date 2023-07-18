3e2d722fd96a40e296718f2f130117fe66d42dac
Bobbi Towers <btowers793@gmail.com>
2023-07-18 16:34:15 -0700

    use regular functions in example solution

--- demo/main.js
+++ demo/main.js
@@ -4,29 +4,15 @@ import { EditorState } from '@codemirror/state'
 import { clojure } from "../src/clojure"
 
 let editorState = EditorState.create({
-    doc: `(defn new-list [] '())
-
-(defn add-language [lang-list lang] 
-  (conj lang-list lang))
-
-(defn first-language [lang-list] 
-  (first lang-list))
-
-(defn remove-language [lang-list] 
-  (rest lang-list))
-
-(defn count-languages [lang-list]
-  (count lang-list))
-
-(defn learning-list []
-  (-> (new-list)
-      (add-language "Clojure")
-      (add-language "Lisp")
-      remove-language
-      (add-language "Java")
-      (add-language "JavaScript")
-      count-languages))
-
+    doc: `(defn learning-list []
+  (-> '()
+      (conj "Clojure")
+      (conj "Lisp")
+       rest
+      (conj "Java")
+      (conj "JavaScript")
+       count))
+      
 (learning-list)`,
     extensions: [basicSetup, clojure()]
 })
