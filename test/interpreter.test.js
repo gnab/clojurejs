e1737011f068baf7a8076a0edb298f5443c26af1
Bobbi Towers <btowers793@gmail.com>
2023-07-19 19:42:13 -0700

    test READ_STR of strings

--- test/interpreter.test.js
+++ test/interpreter.test.js
@@ -16,3 +16,17 @@ test('read of symbols', () => {
   expect(read_str('abc').value).toBe('abc')
   expect(read_str('.').value).toBe('.')
 })
+
+test('READ_STR of strings', () => {
+  expect(read_str('"a string"')).toBe('a string')
+  expect(read_str('"a string (with parens)"')).toBe('a string (with parens)')
+  expect(read_str('"a string"()')).toBe('a string')
+  expect(read_str('"a string"123')).toBe('a string')
+  expect(read_str('"a string"abc')).toBe('a string')
+  expect(read_str('""')).toBe('')
+  expect(read_str('"abc "')).toBe('abc ')
+  expect(read_str('" abc"')).toBe(' abc')
+  expect(read_str('"$abc"')).toBe('$abc')
+  expect(read_str('"abc$()"')).toBe('abc$()')
+  expect(read_str('"\\"xyz\\""')).toBe('"xyz"')
+})
